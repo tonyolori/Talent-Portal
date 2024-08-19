83053e357b5ca383fce7e6214c5d24c6476f9b38
Tony O <anthony.olori@reventtechnologies.com>
2024-08-19 09:52:00 +0100

    updated gitIgnore to remove all obj directories

--- Src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6fec4c3b8d51d330e1699cd81b0f2d2e6ff9800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f060b55533c3307ce0fcd2e980c144e03311aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
